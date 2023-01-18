1ff861cabd8f9d651c85d0096361aa2e3be687ee
hyemib <hyem0127@gmail.com>
2023-01-18 18:38:55 +0900

    [feat] 상품 뷰 및 예약 뷰

---
+++ Bottles_V2/View/Bottle/ReservedView.swift
@@ -0,0 +1,45 @@
+//
+//  ReservedView.swift
+//  Bottles_V2
+//
+//  Created by hyemi on 2023/01/18.
+//
+
+import SwiftUI
+
+struct ReservedView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            // 예약 상품 이미지
+            ZStack {
+               Circle()
+                    .fill(Color(UIColor(red: 246/255, green: 243/255, blue: 238/255, alpha: 1.0)))
+                    .frame(width: 221, height: 221)
+                Image("kilchoman")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 180)
+                    .clipShape(Circle())
+            
+            }
+            
+            VStack {
+                Text("예약이 완료되었습니다.")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                Text("곧 예약 확정 알림을 보내드릴게요!")
+                    .font(.title3)
+                    .fontWeight(.bold)
+            }
+            
+            Spacer()
+        }
+        .offset(y: 100)
+    }
+}
+
+struct ReservedView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReservedView()
+    }
+}
