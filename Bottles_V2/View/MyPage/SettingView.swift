2ac4bdf8ed6be745f3a5d05c56e54e142d289bb7
FrediOS-dev <114239407+FrediOS-dev@users.noreply.github.com>
2023-01-18 13:23:55 +0900

    [Feat] MyPageView, SettingView 목업

---
+++ Bottles_V2/View/MyPage/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  Bottles_V2
+//
+//  Created by BOMBSGIE on 2023/01/18.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
