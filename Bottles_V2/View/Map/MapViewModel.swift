478d1baa52cd2f5f5ec27887c29113780d5a3a2c
Changhyun Kang <changhyun.kyle@gmail.com>
2023-01-20 18:00:04 +0900

    [Refactor] 네이버 지도 뷰모델 리팩토링

--- Bottles_V2/View/Map/MapViewModel.swift
+++ Bottles_V2/View/Map/MapViewModel.swift
@@ -6,15 +6,43 @@
 //
 
 import SwiftUI
+import CoreLocation
 
-struct MapViewModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager?
+    @Published var coord = (0.0, 0.0)
+//    @Published var foodCarts = foodCartDummy
+    
+    func checkIfLocationServicesIsEnabled() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            checkLocationAuthorization()
+        } else {
+            print("Show an alert letting them know this is off and to go turn i on.")
+        }
     }
-}
-
-struct MapViewModel_Previews: PreviewProvider {
-    static var previews: some View {
-        MapViewModel()
+    
+    func checkLocationAuthorization() {
+        guard let locationManager = locationManager else { return }
+        
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Your location is restricted likely due to parental controls.")
+        case .denied:
+            print("You have denied this app location permission. Go into setting to change it.")
+        case .authorizedAlways, .authorizedWhenInUse:
+            coord = (Double(locationManager.location?.coordinate.latitude ?? 0.0), Double(locationManager.location?.coordinate.longitude ?? 0.0))
+            print("GPS 권한 설정 완료")
+            print("coord : \(coord)")
+        @unknown default:
+            break
+        }
     }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+     }
 }
