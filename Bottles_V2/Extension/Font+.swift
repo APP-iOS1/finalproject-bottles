016cb61b951a82e7b9b5277d3eb68576bc975410
Changhyun Kang <changhyun.kyle@gmail.com>
2023-01-20 15:18:25 +0900

    [Add] 스포카 한 산스 네오 폰트 추가

---
+++ Bottles_V2/Extension/Font+.swift
@@ -0,0 +1,21 @@
+//
+//  Font+.swift
+//  Bottles_V2
+//
+//  Created by 강창현 on 2023/01/20.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static let bottles20: Font = .custom("SpoqaHanSansNeo-Regular", size: 20)
+    static let bottles19: Font = .custom("SpoqaHanSansNeo-Regular", size: 19)
+    static let bottles18: Font = .custom("SpoqaHanSansNeo-Regular", size: 18)
+    static let bottles17: Font = .custom("SpoqaHanSansNeo-Regular", size: 17)
+    static let bottles16: Font = .custom("SpoqaHanSansNeo-Regular", size: 16)
+    static let bottles15: Font = .custom("SpoqaHanSansNeo-Regular", size: 15)
+    static let bottles14: Font = .custom("SpoqaHanSansNeo-Regular", size: 14)
+    static let bottles13: Font = .custom("SpoqaHanSansNeo-Regular", size: 13)
+    static let bottles12: Font = .custom("SpoqaHanSansNeo-Regular", size: 12)
+}
---
+++ Bottles_V2/View/Map/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Bottles_V2
+//
+//  Created by 강창현 on 2023/01/20.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
