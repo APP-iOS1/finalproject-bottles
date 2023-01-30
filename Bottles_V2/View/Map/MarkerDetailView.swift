99b24e5b12f5597e9add022d17d74e39537fcd52
Changhyun Kang <changhyun.kyle@gmail.com>
2023-01-30 14:41:43 +0900

    [Add] Font Extension Bottles11 추가

---
+++ Bottles_V2/View/Map/MarkerDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  MarkerDetailView.swift
+//  Bottles_V2
+//
+//  Created by 강창현 on 2023/01/30.
+//
+
+import SwiftUI
+
+struct MarkerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MarkerDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MarkerDetailView()
+    }
+}
