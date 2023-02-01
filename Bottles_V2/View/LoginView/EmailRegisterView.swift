327e9acbf01cf610e58c1b5d0c80b196ea837acd
FrediOS-dev <114239407+FrediOS-dev@users.noreply.github.com>
2023-02-02 01:50:48 +0900

    [Feat] EmailRegisterView 목업 완료 TODO: 디테일 수정해야함

--- Bottles_V2/View/LoginView/EmailRegisterView.swift
+++ Bottles_V2/View/LoginView/EmailRegisterView.swift
@@ -15,12 +15,14 @@ struct EmailRegisterView: View {
     let passwordExpression: String = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,15}$"
     
     @State var registerEmail: String = ""
+    @State var verificationCode: String = ""
     @State var registerPassword: String = ""
     @State var passwordCheck: String = ""
     @State var nickname: String = ""
     @State var phoneNumber: String = ""
     
-    /// 이메일 양식이 올바르지 않으면 사용자에게 뷰에서 에러를 표현해주는 변수
+    
+    /// 중복확인 버튼을 누르고 이메일이 중복일 때 텍스트 필드 애니메이션을 활성화 시켜주는 변수
     @State private var emailError: Bool = false
     
     /// 비밀번호 양식이 올바르지 않으면 사용자에게 뷰에서 에러를 표현해주는 변수
@@ -29,32 +31,40 @@ struct EmailRegisterView: View {
     /// 비밀번호 확인이 같지 않으면 사용자에게 뷰에서 에러를 표현해주는 변수
     @State private var checkingPasswordError: Bool = false
     
-    /// 뷰에 이메일 형식이 맞는지 띄워주는 연산 프로퍼티
-    private var resultEmailText: String {
-        if registerEmail == "" {
-            return ""
-        } else {
-            return emailNotFitFormat ? "올바른 이메일 형식이 아닙니다." : "사용 가능한 이메일 입니다."
-        }
-    }
+    /// 중복확인 버튼을 누르면 이메일 인증 번호 입력하는 창 띄워주는 변수
+    @State private var isShowingVerificationCode: Bool = false
     
-    /// 뷰에 비밀번호 형식이 맞는지 띄워주는 연산 프로퍼티
-    private var resultPasswordText: String {
-        if registerPassword == "" {
-            return ""
-        } else {
-            return passwordNotFitFormat ? "영어, 숫자, 특수문자 포함 8~15자리" : "사용 가능한 비밀번호 입니다."
-        }
-    }
+    /// 이용약관 전체 동의 변수
+    @State var allAgreement: Bool = false
     
-    /// 뷰에 비밀번호 확인이 비밀번호 입력한 값과 맞는지 띄워주는 연산프로퍼티
-    private var resultPasswordCheckText: String {
-        if passwordCheck == "" {
-            return ""
-        } else {
-            return passwordCheckFail ? "비밀번호가 일치하지 않습니다." : "비밀번호가 일치합니다."
-        }
-    }
+    /// 첫번째 이용약관 동의 변수
+    @State var firstAgreement: Bool = false
+    
+    /// 개인정보 수집 이용 동의 변수
+    @State var secondAgreement: Bool = false
+    
+    /// 만 19세 이상 동의 변수
+    @State var thirdAgreement: Bool = false
+    
+    /// 이용약관 버튼 눌렀을 때 이동하는 웹링크 배열
+    let agreementURLs: [String] = [
+        "https://www.youtube.com/", // 이용약관 동의 링크
+        "https://line.me/ko/", // 개인정보 수집·이용 동의 링크
+        "https://www.kakaocorp.com/page/" // 만 19세 이상 동의 링크
+    ]
+    
+    /// 이용약관 동의 필드의 텍스트 배열
+    let agreementTitles: [String] = [
+        "이용약관 동의",
+        "개인정보 수집·이용 동의",
+        "본인은 만 19세 이상입니다."
+    ]
+    
+    /// SafariWebView에 바인딩으로 넘겨줄 웹 링크 각 버튼마다 눌렀을 때 링크가 변경된다.
+    @State var selectedAgreementWebLink: URL = URL(string: "www.naver.com")!
+    
+    /// SafariWebView 시트로 띄우는 변수
+    @State private var isShowingSheet: Bool = false
     var body: some View {
         ScrollView {
             // MARK: - email 입력창
@@ -82,12 +92,37 @@ struct EmailRegisterView: View {
                         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5){
                             emailError = false
                         }
+                        // 이메일 인증 번호 입력창 활성화
+                        isShowingVerificationCode.toggle()
                     }){
                        Text("중복확인")
                             .modifier(EmailViewButtonModifier(width: 100, height: 48))
                     }
                 }
+            }
             
+            //MARK: - 이메일 인증번호 입력창
+            if isShowingVerificationCode {
+                Group {
+                    HStack(alignment: .bottom) {
+                        Text("이메일 인증")
+                            .font(.bottles14)
+                        + Text("*")
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                    }
+                    .padding(.horizontal, 20)
+                    HStack{
+                        TextField("인증번호를 입력해주세요.", text: $verificationCode)
+                            .modifier(LoginTextFieldModifier(width: 225, height: 48))
+                        Button(action: {
+                            //TODO: 이메일 인증번호 확인 로직
+                        }){
+                            Text("인증번호 받기")
+                                .modifier(EmailViewButtonModifier(width: 125, height: 48))
+                        }
+                    }
+                }
             }
             
             // MARK: - 비밀번호 입력창
@@ -97,8 +132,8 @@ struct EmailRegisterView: View {
                         .font(.bottles14)
                     + Text("*")
                         .foregroundColor(.accentColor)
-                    // 뷰에 비밀번호 형식이 맞는지 틀린지 사용자가 볼 수 있게 Text로 띄워준다.
                     Spacer()
+                    // 뷰에 비밀번호 형식이 맞는지 틀린지 사용자가 볼 수 있게 Text로 띄워준다.
                     Text("\(resultPasswordText)")
                         .font(.bottles12)
                         .foregroundColor(passwordNotFitFormat ? .secondary : .green)
@@ -153,6 +188,89 @@ struct EmailRegisterView: View {
                     .modifier(LoginTextFieldModifier(width: 357, height: 48))
                     .keyboardType(.numberPad)
             }
+            
+            // MARK: - 이용약관동의
+            Group {
+                HStack {
+                    Text("이용약관동의")
+                        .font(.bottles14)
+                    + Text("*")
+                        .foregroundColor(.accentColor)
+                    Spacer()
+                }
+                .padding(20)
+                // MARK: - 이용약관 전체동의
+                Button(action: {
+                    
+                    allAgreement.toggle()
+                }){
+                    HStack{
+                        Image(systemName: "checkmark.circle")
+                            .font(.title2)
+                            .foregroundColor(allAgreement ? .accentColor : .secondary)
+                        Text("전체 동의합니다.")
+                            .font(.bottles19)
+                            .foregroundColor(.primary)
+                            .fontWeight(.semibold)
+                        Spacer()
+                    }
+                    .padding(.horizontal, 20)
+                    
+                }
+                Divider()
+                    .padding(.horizontal, 16)
+                // MARK: - 이용약관 동의 체크 버튼
+                HStack {
+                    VStack(spacing: 10) {
+                        Button(action: {
+                            firstAgreement.toggle()
+                        }) {
+                            Image(systemName: "checkmark.circle")
+                                .foregroundColor(allAgreement || firstAgreement ? .accentColor : .secondary)
+                                .font(.title2)
+                        }
+                        Button(action: {
+                            secondAgreement.toggle()
+                        }) {
+                            Image(systemName: "checkmark.circle")
+                                .foregroundColor(allAgreement || secondAgreement ? .accentColor : .secondary)
+                                .font(.title2)
+                        }
+                        Button(action: {
+                            thirdAgreement.toggle()
+                        }) {
+                            Image(systemName: "checkmark.circle")
+                                .foregroundColor(allAgreement || thirdAgreement ? .accentColor : .secondary)
+                                .font(.title2)
+                        }
+                    }
+                    //MARK: - 이용약관 텍스트 눌렀을 때 웹으로 이동
+                    VStack (alignment: .leading, spacing: 20){
+                        ForEach(0 ..< agreementURLs.count, id: \.self){ index in
+                            Button(action: {
+                                selectedAgreementWebLink = URL(string: "\(agreementURLs[index])")!
+                                isShowingSheet.toggle()
+                            }){
+                                HStack{
+                                    Text("\(agreementTitles[index]) ")
+                                        .foregroundColor(.primary)
+                                    + Text("(필수)")
+                                        .foregroundColor(.secondary)
+                                    Spacer()
+                                    Image(systemName: "chevron.forward")
+                                        .foregroundColor(.secondary)
+                                }
+                            }
+                            .font(.bottles12)
+                            .sheet(isPresented: $isShowingSheet) {
+                                SafariWebView(selectedUrl: $selectedAgreementWebLink)
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 30)
+            }
             Button(action: {
                 // TODO: 이메일로 회원가입 로직 넣기
             }){
@@ -161,8 +279,13 @@ struct EmailRegisterView: View {
             }
             
         }
+//        .sheet(isPresented: $isShowingSheet) {
+//            SafariWebView(selectedUrl: selectedAgreementWebLink)
+//        }
         
     }
+    
+    //MARK: - 뷰에 사용되는 연산프로퍼티들
     
     /// 이메일이 정규식에 맞는지 확인 해주는 연산프로퍼티
     private var emailNotFitFormat: Bool {
@@ -178,6 +301,34 @@ struct EmailRegisterView: View {
     private var passwordCheckFail: Bool {
         passwordCheck != registerPassword
     }
+    
+    /// 뷰에 이메일 형식이 맞는지 텍스트로 띄워주는 연산 프로퍼티
+    private var resultEmailText: String {
+        if registerEmail == "" {
+            return ""
+        } else {
+            return emailNotFitFormat ? "올바른 이메일 형식이 아닙니다." : "사용 가능한 이메일 입니다."
+        }
+    }
+    
+    /// 뷰에 비밀번호 형식이 맞는지 텍스트로 띄워주는 연산 프로퍼티
+    private var resultPasswordText: String {
+        if registerPassword == "" {
+            return ""
+        } else {
+            return passwordNotFitFormat ? "영어, 숫자, 특수문자 포함 8~15자리" : "사용 가능한 비밀번호 입니다."
+        }
+    }
+    
+    /// 뷰에 비밀번호 확인이 비밀번호 입력한 값과 맞는지 텍스트로 띄워주는 연산프로퍼티
+    private var resultPasswordCheckText: String {
+        if passwordCheck == "" {
+            return ""
+        } else {
+            return passwordCheckFail ? "비밀번호가 일치하지 않습니다." : "비밀번호가 일치합니다."
+        }
+    }
+    
 }
 
 
