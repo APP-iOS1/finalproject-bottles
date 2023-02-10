9fd96d22d909c86cce76f9c7e34c9823141b3c6d
Changhyun Kang <changhyun.kyle@gmail.com>
2023-02-10 18:24:35 +0900

    [Feat] 검색 로직 및 검색 리스트 구현

--- Bottles_V2/View/Map/MapSearchView.swift
+++ Bottles_V2/View/Map/MapSearchView.swift
@@ -12,11 +12,17 @@ struct MapSearchView: View {
     enum FocusField: Hashable {
         case field
     }
-    @State var searchBarText: String = "" // 텍스트필드에 입력되는 텍스트
+    @Binding var searchBarText: String // 텍스트필드에 입력되는 텍스트
     @State var doneTextFieldEdit: Bool = false
-    @FocusState var focusField: FocusField? // 포커스된 텍스트필드
+    @FocusState var focus: Bool  // 포커스된 텍스트필드
     @Binding var tapped: Bool // MapView에서의 텍스트 필드의 tap여부를 바인딩으로 넘김
-    
+    @EnvironmentObject var mapViewModel: MapViewModel
+    @EnvironmentObject var shopDataStore: ShopDataStore
+    @Binding var showMarkerDetailView: Bool
+//    @Binding var mapSearchBarText: String
+    @Binding var searchResult: [ShopModel]
+    @Binding var currentShopId: String
+    @Binding var tapSearchButton: Bool
     var body: some View {
         NavigationStack {
             ZStack {
@@ -39,50 +45,93 @@ struct MapSearchView: View {
                                 })
                                 .padding(5)
                             
-                            HStack {
-                                Image(systemName: "magnifyingglass")
-                                    .foregroundColor(.black)
-                                    .font(.system(size: 18))
-                                // 검색바 TextField
-                                // onEditingChanged : TextField 편집 상태를 bool값으로 받는다
-                                // onCommit : TextField 작업 중 Return 키를 누를 때 수행할 작업
-                                TextField("원하는 술, 바틀샵 검색", text: $searchBarText, onEditingChanged: { bool in
-                                    if bool {
-                                        doneTextFieldEdit = false
-                                    } else {
+                            MapViewSearchBar(showMarkerDetailView: $showMarkerDetailView, searchBarText: $searchBarText, searchResult: $searchResult, currentShopId: $currentShopId, tapSearchButton: $tapSearchButton)
+                            
+                            //                            HStack {
+                            //                                Image(systemName: "magnifyingglass")
+                            //                                    .foregroundColor(.black)
+                            //                                    .font(.system(size: 18))
+                            //                                // 검색바 TextField
+                            //                                // onEditingChanged : TextField 편집 상태를 bool값으로 받는다
+                            //                                // onCommit : TextField 작업 중 Return 키를 누를 때 수행할 작업
+                            //                                TextField("원하는 술, 바틀샵 검색", text: $searchBarText, onEditingChanged: { bool in
+                            //                                    if bool {
+                            //                                        doneTextFieldEdit = false
+                            //                                    } else {
+                            //                                        doneTextFieldEdit = true
+                            //                                    }
+                            //                                }, onCommit: {
+                            //                                    doneTextFieldEdit = true
+                            //                                })
+                            //                                .font(.bottles16)
+                            //                                .multilineTextAlignment(.leading)
+                            //                                .submitLabel(.search)
+                            //                                .focused($focusField, equals: .field)
+                            //
+                            //                                if !searchBarText.isEmpty {
+                            //                                    Button(action: {
+                            //                                        self.searchBarText = ""
+                            //                                        //focus = false
+                            //                                    }) {
+                            //                                        Image(systemName: "xmark.circle.fill")
+                            //                                    }
+                            //                                } else {
+                            //                                    EmptyView()
+                            //                                }
+                            //                            }
+                            //                                .padding(10)
+                            //                                .frame(width: 250, height: 35)
+                            
+                            //                                .cornerRadius(12)
+                        }
+                        //                        .frame(width: 300)
+                        .padding(10)
+                        .offset(y: 30)
+                        //                        Spacer()
+                        // 여기서 검색 리스트 구현
+                        List {
+                            ForEach (searchResult, id: \.self) { item in
+                                // 검색어와 겹치는 단어가 있는지 없는지 확인
+                                if item.shopName.replacingOccurrences(of: " ", with: "").contains(searchBarText.replacingOccurrences(of: " ", with: "")) {
+                                    Button {
                                         doneTextFieldEdit = true
+                                        
+                                        // 사용자가 리스트에서 찾고자하는 단어가 있어 터치 시, 해당 단어를 검색창의 텍스트로 전환
+                                        searchBarText = item.shopName
+                                        
+                                    } label: {
+                                        HStack {
+                                            Image(systemName: "magnifyingglass")
+                                                .foregroundColor(.gray)
+                                                .padding([.leading, .trailing])
+                                            
+                                            // 검색어와 겹치는 단어가 있는 bottleName의 경우, 검색어와 겹치는 단어들만 accentColor
+                                            // 현재는 shop을 제외한 bottleName만 리스트에 보임
+                                            Text(item.shopName) { string in
+                                                if let range = string.range(of: searchBarText.trimmingCharacters(in: .whitespaces)) {
+                                                    string[range].foregroundColor = Color("AccentColor")
+                                                }
+                                            }
+                                            .font(.bottles16)
+                                            Spacer()
+                                            
+                                            Image("Map_tab_fill")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 16, height: 16)
+                                            
+                                        }
                                     }
-                                }, onCommit: {
-                                    doneTextFieldEdit = true
-                                })
-                                .font(.bottles16)
-                                .multilineTextAlignment(.leading)
-                                .submitLabel(.search)
-                                .focused($focusField, equals: .field)
-                                
-                                if !searchBarText.isEmpty {
-                                    Button(action: {
-                                        self.searchBarText = ""
-                                        //focus = false
-                                    }) {
-                                        Image(systemName: "xmark.circle.fill")
-                                    }
-                                } else {
-                                    EmptyView()
                                 }
                             }
-                            .padding(10)
-                            .frame(width: 250, height: 35)
-                            .background{
-                                Color.gray_f7
-                            }
-                            .cornerRadius(12)
                         }
+                        .padding(10)
+                        .listStyle(.plain)
                     }
                     // 해당 텍스트필드를 포커스로 키보드가 자동으로 올라오게 함
                     .onAppear {
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                            focusField = .field
+                            focus = true
                         }
                     }
                 } else {
@@ -96,9 +145,7 @@ struct MapSearchView: View {
             }
         }
         .edgesIgnoringSafeArea(.all)
-        
     }
-        
 }
 
 //struct MapSearchView_Previews: PreviewProvider {
