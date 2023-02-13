8b7ace650713aca91a1750dd5cc04084d1ed81fa
Da01002 <80445363+Da01002@users.noreply.github.com>
2023-02-13 22:33:06 +0900

    [Design] MyPageView 디자인 수정

--- Bottles_V2/View/MyPage/PickUpListCell.swift
+++ Bottles_V2/View/MyPage/PickUpListCell.swift
@@ -15,22 +15,23 @@ struct PickUpListCell: View {
         VStack{
             // MARK: - 예약 일자, 예약 상태 HStack
             HStack {
-                Text("2023.01.18")
-                    .font(.bottles15)
-                    .bold()
+                Text("\(reservationData.reservedTime)")
+                    .font(.bottles16)
+                    //.bold()
                 Spacer()
                 
-                Text("\(reservationData.state)")
+                Text("예약접수")
+                //Text("\(reservationData.state)")
                     .font(.bottles12)
                     .overlay{
                         RoundedRectangle(cornerRadius: 15)
                             .stroke()
-                            .frame(width: 80, height: 30)
+                            .frame(width: 61, height: 23)
                     }
                     .foregroundColor(.gray)
                     .padding(.trailing, 10)
             }
-            .padding(.vertical)
+            .padding(.vertical, 5)
             
             // MARK: - 픽업 매장, 상품명 HStack
             HStack{
@@ -39,22 +40,26 @@ struct PickUpListCell: View {
                         .padding(.bottom, -3)
                     Text("상품 명")
                 }
+                .padding(.trailing)
                 VStack(alignment:.leading){
-                    Text("\(reservationData.shopID)")
+                    Text("test")
+                    //Text("\(reservationData.shopID)")
                         .padding(.bottom, -3)
                     Text("디 오리지널 골드바 위스키")
                 }
                 Spacer()
             }
-            .font(.bottles13)
+            .font(.bottles14)
         }
         .foregroundColor(.black)
         .padding(.horizontal)
+        .padding(.bottom, 5)
+        .padding(.top, 5)
     }
 }
 
-struct PickUpListCell_Previews: PreviewProvider {
-    static var previews: some View {
-        PickUpListCell()
-    }
-}
+//struct PickUpListCell_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PickUpListCell()
+//    }
+//}
--- Bottles_V2/View/MyPage/PickUpListView.swift
+++ Bottles_V2/View/MyPage/PickUpListView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct PickUpListView: View {
     
     @EnvironmentObject var reservationDataStore: ReservationDataStore
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
         VStack{
@@ -17,17 +18,29 @@ struct PickUpListView: View {
                 // TODO: 예약 리스트 배열을 넣어 순회해줘야 함
                 ForEach(reservationDataStore.reservationData) { reservationData in
                     NavigationLink(destination: PickUpDetailView()){
-                        PickUpListCell()
+                        PickUpListCell(reservationData: reservationData)
                     }
-                    Rectangle()
-                        .frame(height: 7)
-                        .foregroundColor(Color("lightGray"))
-                        .padding(.bottom, -6)
+                    Divider()
+
                 }
             }
         }
         .navigationTitle("예약 내역")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: backButton)
     }
+    
+    var backButton : some View {
+        Button(
+            action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "chevron.backward")    // back button 이미지
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color.black)
+            }
+    }
+
 }
 
 struct PickUpListView_Previews: PreviewProvider {
